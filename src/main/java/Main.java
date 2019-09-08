6b92cf69d61a593bb3cfc94c09293090debb195b
Hp <aasd>
2019-09-08 12:42:59 +0200

    1st commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Desktop\\ChromeDriver75.exe");
         ChromeDriver driver = new ChromeDriver();
@@ -14,6 +14,7 @@ public class Main {
         System.out.println(driver.getCurrentUrl());
         WebElement element = driver.findElement(By.className("sb-destination__input"));
 
-
+        // jeśli w terminalu po git commit nic sie nie pojawi, to wpisuje nazwe commit i naciskam ":" w(save) q(exit)
+        // new comment
     }
 }
